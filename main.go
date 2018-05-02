bfcec7d327ecb7a2a00035bf9e34dbbbb51ec7b1
Zack <zack.scholl@gmail.com>
2018-05-02 16:00:01 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+)
+
+func main() {
+	args := os.Args
+	if len(args) > 1 {
+		if args[1] == "pull" || args[1] == "clone" {
+			args = append(args[:2], append([]string{"--depth=1"}, args[2:]...)...)
+		}
+	}
+	args[0] = "git"
+	cmd := exec.Command(args[0])
+	if len(args) > 0 {
+		cmd = exec.Command(args[0], args[1:]...)
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		os.Exit(1)
+	}
+}
