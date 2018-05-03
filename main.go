26b61907777d5db94fd3737d7aa580ef8522d275
Zack <zack.scholl@gmail.com>
2018-05-02 17:59:06 -0700

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	args := os.Args
 	if len(args) > 1 {
 		if args[1] == "pull" || args[1] == "clone" {
-			args = append(args[:2], append([]string{"--depth=1", "--shallow-submodules", "--no-tags", "--single-branch"}, args[2:]...)...)
+			args = append(args[:2], append([]string{"--depth=1", "--shallow-submodules","--single-branch"}, args[2:]...)...)
 		}
 	}
 	args[0] = "/usr/bin/git"
